3a3bd62bf7d278b5b1adee607269db442aa6007b
MICHAEL J MELTON <meltonm83@gmail.com>
2016-09-02 16:02:45 -0700

    Reverts server.js to original form

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res) {
-    var result = 'App is running'
-    response.send(result);
-});
+app.use(express.static(__dirname + '/'));
 
-const port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number);
+app.listen(process.env.PORT || 8080);
