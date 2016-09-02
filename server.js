3b4e1d07c3c4bd0e9b4342a44c025d2789c0cc2c
MICHAEL J MELTON <meltonm83@gmail.com>
2016-09-02 15:44:42 -0700

    Revises get request

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/'));
+app.get('/', function(req, res) {
+    var result = 'App is running'
+    response.send(result);
+});
 
 const port_number = server.listen(process.env.PORT || 3000);
 app.listen(port_number);
